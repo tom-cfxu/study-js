8bac504116aea24fe2a856a62177c34ebf6ffef2
tom <1157164295@qq.com>
2020-07-27 17:44:11 +0800

    12332

--- 10/script.js
+++ 10/script.js
@@ -9,16 +9,21 @@ const musicContainer = $('music-container'),
 
     audio = $('audio'),
     progress = $('progress'),
+    progress2 = $('progress2'),
     progressContainer = $('progress-container'),
     title = $('title'),
-    cover = $('cover');
+    cover = $('cover'),
+    volumeBtn = $('volume'),
+    progressVolumeContainer = $('progress-volume-container'),
+    progressVolume = $('progress-volume');
 
 //歌曲名称
 const songs = ['99RadioService - YOUTHFUL', '99RadioService - STAR', '99RadioService - COLORFUL'];
 
 //创建下标追踪歌曲
 let songIndex = 0;  //默认歌曲下标
-const volume = 0.5; //默认音量
+let volume = 0.5; //默认音量
+let currentVolume;
 //初始化页面时加载歌曲到DOM节点中
 loadSong(songs[songIndex]);
 //暂停
@@ -70,7 +75,6 @@ function setProgress(e) {
     // console.log(clickX);
     const duration = audio.duration;
     audio.currentTime = (clickX / width) * duration;
-
 }
 //loadSong函数
 function loadSong(song) {
@@ -88,14 +92,72 @@ playBtn.addEventListener("click", () => {
         playSong();
     }
 })
+//更新音量
+function updateVolumeProgress(e) {
+    // console.log(e)
+    const volume = e.srcElement.volume;
+    progress2.style.height = `${volume * 100}%`;
+    switch (volume) {
+        case 0:
+            volumeBtn.querySelector('i.fa').classList.remove('fa-volume-up');
+            volumeBtn.querySelector('i.fa').classList.remove('fa-volume-dowm');
+            volumeBtn.querySelector('i.fa').classList.add('fa-volume-off');
+            break;
+        case 1:
+            volumeBtn.querySelector('i.fa').classList.remove('fa-volume-dowm');
+            volumeBtn.querySelector('i.fa').classList.remove('fa-volume-off');
+            volumeBtn.querySelector('i.fa').classList.add('fa-volume-up');
+            break;
+        default:
+            volumeBtn.querySelector('i.fa').classList.remove('fa-volume-up');
+            volumeBtn.querySelector('i.fa').classList.remove('fa-volume-off');
+            volumeBtn.querySelector('i.fa').classList.add('fa-volume-dowm');
+            break;
+    }
+}
+//点击音量
+function setVolumeProgress(e) {
+    console.log(e);
+    const height = this.clientHeight;
+    const clickY = e.offsetY;
+    console.log(height, clickY)
+    audio.volume = ((height - clickY) / height);
+    audio.volume = (clickY / height);
+}
+//修改音量图标
+function setVolume(v) {
+    audio.volume = v;
+}
+
+//音量进度条点击事件
+progressVolume.addEventListener('click', setVolumeProgress)
+//volumeBtn监听事件
+volumeBtn.addEventListener('click', () => {
+    //保存当前音量
+    // console.log(audio.volume);
+    if (audio.volume > 0) {
+        currentVolume = audio.volume;
+        setVolume(0);
+    } else {
+        setVolume(currentVolume);
+    }
+})
+volumeBtn.addEventListener('mouseover', () => {
+    progressVolumeContainer.classList.add('show');
+    // console.log(progressVolumeContainer)
+    musicContainer.addEventListener('mouseleave', () => {
+        progressVolumeContainer.classList.remove('show');
+    })
+})
 //切换歌曲
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 //更新进度条
-audio.addEventListener("timeupdate", updateProgress)
-
+audio.addEventListener("timeupdate", updateProgress);
+//更新音量
+audio.addEventListener('volumechange', updateVolumeProgress);
 //点击进度条容器,更新歌曲播放
-progressContainer.addEventListener("click", setProgress)
+progressContainer.addEventListener("click", setProgress);
 
 //播放结束自动切换
-audio.addEventListener('ended', nextSong)
+audio.addEventListener('ended', nextSong);
